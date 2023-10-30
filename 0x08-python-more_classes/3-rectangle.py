2f845bc4239f506765da717580f6117fd6681231
abdallahade1 <abdoadelshabaan@gmail.com>
2023-10-30 14:02:23 -0700

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+This is a module for a class Rectangle
+"""
+
+
+class Rectangle:
+    """Class of a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if alue < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = alue
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate perimeter"""
+        width = self.__width
+        height = self.__height
+        if width == 0 or height == 0:
+            return 0
+        return (width + height) * 2
+
+    def __str__(self):
+        """returns printable string representaion of the rectangle"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
