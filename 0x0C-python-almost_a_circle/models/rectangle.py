85dc31a7f841389d2566151383779522f8889903
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-13 15:09:20 -0800

    models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Model 2"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Inheriting from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializing class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """retrieve x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """retrieve y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        self.__y = value
