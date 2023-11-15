939324e00659fc3f9e67080d105233106b49a148
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-15 09:04:52 -0800

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Model 1"""
 import json
 
+
 class Base:
     """the base class"""
     __nb_objects = 0
@@ -21,3 +22,12 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes Json representation"""
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w",
+                  encoding="utf-8") as file:
+            file.write(cls.to_json_string(list_objs))
