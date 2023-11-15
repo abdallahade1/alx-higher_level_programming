c5d26c9f5d05a3d215e02655f7482ac072476829
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-14 16:11:46 -0800

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,15 +96,20 @@ class Rectangle(Base):
                                                        self.__width,
                                                        self.__height)
 
-    def update(self, *args, ):
+    def update(self, *args, **kwargs):
         """Updates the Rectangle instance with the given arguments."""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            elif len(args) > 1:
+                self.width = args[1]
+            elif len(args) > 2:
+                self.height = args[2]
+            elif len(args) > 3:
+                self.x = args[3]
+            elif len(args) > 4:
+                self.y = args[4]
+
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
