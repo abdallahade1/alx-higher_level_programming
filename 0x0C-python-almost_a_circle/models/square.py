fff8c8cd308dbb4e6763432cccaef2296de504c7
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-15 08:11:41 -0800

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,20 @@ class Square(Rectangle):
         """set width and height with the same value, value"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigning attributes"""
+        if args:
+            """check for args"""
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            """if args is empty, use kwargs"""
+            for key, value in kwargs.items():
+                setattr(self, key, value)
