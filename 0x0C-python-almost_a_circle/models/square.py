dc28d32be72e114eab1e72de2af055e183c09408
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-15 08:41:25 -0800

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,3 +43,13 @@ class Square(Rectangle):
             """if args is empty, use kwargs"""
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns the dictionary representation"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
