108805c9c57a407a354ead50cc304a08077a130e
abdallahade1 <abdoadelshabaan@gmail.com>
2024-01-19 07:06:03 -0800

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Get states from the database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Get states from the database by accessing it"""
+
+    con = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states")
+
+    selected_rows = cur.fetchall()
+
+    for rows in selected_rows:
+        print(rows)
