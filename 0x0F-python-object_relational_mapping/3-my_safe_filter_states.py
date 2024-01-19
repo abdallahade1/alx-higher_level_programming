1aa5d34372a4f20eac7325be1b606fdf026fbef5
abdallahade1 <abdoadelshabaan@gmail.com>
2024-01-19 09:37:52 -0800

    Task 2

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Get the states from the database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+import sys
+
+if __name__ == '__main__':
+    """
+    Get the states from the database by accessing it.
+    """
+    conn = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+
+    query = """ SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC """
+
+    cur.execute(query, (sys.argv[4],))
+    rows_selected = cur.fetchall()
+
+    for row in rows_selected:
+        print(row)
+
+    cur.close()
+    conn.close()
