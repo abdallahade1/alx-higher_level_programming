92205e6d2c80f3a49ac7e8da01e2c4dbc09efe71
abdallahade1 <abdoadelshabaan@gmail.com>
2024-01-26 15:45:44 -0800

    Task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode('ascii')
 
-
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
