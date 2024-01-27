07e4d3e1b0a74e9e334389f9b374dd75d3164dda
abdallahade1 <abdoadelshabaan@gmail.com>
2024-01-26 16:20:34 -0800

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,11 +9,11 @@ displays the body of the response (decoded in utf-8).
 import sys
 import requests
 
+
 if __name__ == "__main__":
-
     URL = sys.argv[1]
-    req = requests.get(URL)
 
+    req = requests.get(URL)
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
