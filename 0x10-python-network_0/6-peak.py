dccc528efd083cf74259896ef37736bc542800e4
abdallahade1 <abdoadelshabaan@gmail.com>
2024-01-30 14:06:53 -0800

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,11 +13,11 @@ def find_peak(list_of_integers):
     elif list_int == 2:
         return max(list_of_integers)
 
-    middle = int(list_int / 2)
-    peak = list_of_integers[middle]
-    if peak > list_of_integers[middle - 1] and peak > list_of_integers[middle + 1]:
+    mid = int(list_int / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
-    elif peak < list_of_integers[middle - 1]:
-        return find_peak(list_of_integers[:middle])
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return find_peak(list_of_integers[middle + 1:])
+        return find_peak(list_of_integers[mid + 1:])
