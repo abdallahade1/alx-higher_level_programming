847c169083802b78f7f7ff20b4ccde84b1aaf332
abdallahade1 <abdoadelshabaan@gmail.com>
2024-07-30 21:36:00 +0000

    tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,4 +1,5 @@
-t request = require('request');
+#!/usr/bin/node
+const request = require('request');
 const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(endPoint, function (err, response, body) {
   if (err) {
